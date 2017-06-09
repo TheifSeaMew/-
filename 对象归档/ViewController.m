127dcf0340eb1d33f8da9e58e370746bbe2fd592
TheifSeaMew <1417352977@qq.com>
2017-06-09 14:17:51 +0800

    归档和解归档

--- 对象归档/ViewController.m
+++ 对象归档/ViewController.m
@@ -16,14 +16,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+}
+/////////////////// 第一种归档方式 /////////////////////////
+// 缺点:一个对象一个归档文件,不方便
+// 归档
+- (void)text {
+    NSArray *array = @[@"123" , @"abc" , @"132"];
+    // 拿到创建用户的根路径
+    NSString *homePath = NSHomeDirectory();
+    NSLog(@"%@" , homePath);
+    NSString *path = [homePath stringByAppendingPathComponent:@"text.yaalv"];
+    BOOL success = [NSKeyedArchiver archiveRootObject:array toFile:path];
+    if (success) {
+        NSLog(@"归档成功");
+    }
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 解归档
+- (void)text2 {
+    NSString *homePath = NSHomeDirectory();
+    NSLog(@"%@" , homePath);
+    NSString *path = [homePath stringByAppendingPathComponent:@"text.yaalv"];
+    NSArray *data = [NSKeyedUnarchiver unarchiveObjectWithFile:path];
+    NSLog(@"%@" , data[0]);
 }
 
+// ---------------------- 第二种方法 --------------------------------
+- (void)text3 {
+    NSString *homePath = NSHomeDirectory();
+    NSLog(@"%@" , homePath);
+    NSString *filePath = [homePath stringByAppendingPathComponent:@"custom.text"];
+    NSMutableData *data = [NSMutableData data];
+    NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc] initForWritingWithMutableData:data];
+    [archiver encodeFloat:50.0 forKey:@"weight"];
+    [archiver encodeObject:@"李四" forKey:@"name"];
+    [archiver encodeInt:12 forKey:@"age"];
+    [archiver finishEncoding];
+    
+    BOOL success = [data writeToFile:filePath atomically:YES];
+    if (success) {
+        NSLog(@"------归档成功");
+    }
+}
+// 解归档
+- (void)text4 {
+    NSString *homePath = NSHomeDirectory();
+    NSLog(@"%@" , homePath);
+    NSString *filePath = [homePath stringByAppendingPathComponent:@"custom.text"];
+    NSData *data = [NSData dataWithContentsOfFile:filePath];
+    NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
+    NSString *name = [unarchiver decodeObjectForKey:@"name"];
+    NSLog(@"name:%@" , name);
+}
 
 @end
